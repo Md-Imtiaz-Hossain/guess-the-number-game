d522c5a7b2378e574de8ab25dbc18b6bb82772fc
Imtiaz <imtiaz005123@gmail.com>
2020-11-05 10:37:05 +0600

    5th edit in  console's Main class, add @SpringBootApplication in main class. and run the application.

--- console/src/main/java/com/imtiaz/console/Main.java
+++ console/src/main/java/com/imtiaz/console/Main.java
@@ -1,25 +1,17 @@
 package com.imtiaz.console;
 
-import com.imtiaz.config.GameConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
 
         log.info("Guess The Number Game.");
-
-
-        // === Create context (Container) ===
-        ConfigurableApplicationContext context
-                = new AnnotationConfigApplicationContext(GameConfig.class);
-
-
-        // === close context (Container) ===
-        context.close();
+        SpringApplication.run(Main.class);
 
     }
 }
