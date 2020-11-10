1e08af60a168b7f73f0a33bcb8f68db7203a73fe
Imtiaz <imtiaz005123@gmail.com>
2020-11-10 11:08:57 +0600

    Add a final ViewNames.java class in - web\src\main\java\com\imtiaz\util, and add a constant for home.html called "HOME"...

---
+++ web/src/main/java/com/imtiaz/util/ViewNames.java
@@ -0,0 +1,11 @@
+package com.imtiaz.util;
+
+public final class ViewNames {
+
+    // === constants ===
+    public static final String HOME = "home";
+
+    // === constructors ===
+    private ViewNames() {
+    }
+}
