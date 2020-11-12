f7c1e17087b950b28f9743e9076359e48a46cc41
Imtiaz <imtiaz005123@gmail.com>
2020-11-12 18:31:09 +0600

    Annotate with @Slf4j with MessageServiceImpl class, and create init method and annotate with @PostConstruct.

--- web/src/main/java/com/imtiaz/service/GameServiceImpl.java
+++ web/src/main/java/com/imtiaz/service/GameServiceImpl.java
@@ -2,9 +2,13 @@ package com.imtiaz.service;
 
 import com.imtiaz.Game;
 import com.imtiaz.MessageGenerator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
 @Service
 public class GameServiceImpl implements GameService {
 
@@ -15,6 +19,12 @@ public class GameServiceImpl implements GameService {
     public GameServiceImpl(Game game, MessageGenerator messageGenerator) {
         this.game = game;
         this.messageGenerator = messageGenerator;
+    }
+
+    @PostConstruct
+    public void init(){
+        log.info("number = {}", game.getNumber());
+        log.info("mainMessage = {}", messageGenerator.getMainMessage());
     }
 
     @Override
