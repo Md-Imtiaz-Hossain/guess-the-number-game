47e8a0bf2c3d63eb8c61115e80f49a129a759420
Imtiaz <imtiaz005123@gmail.com>
2020-11-13 11:03:36 +0600

    Add a class GameMappings and add 2 final constant and create constructor in util\GameMappings.java...

---
+++ web/src/main/java/com/imtiaz/util/GameMappings.java
@@ -0,0 +1,13 @@
+package com.imtiaz.util;
+
+public final class GameMappings {
+
+    // === Constants ===
+    public static final String PLAY = "play";
+    public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+
+    // === Constructor ===
+    private GameMappings(){
+
+    }
+}
