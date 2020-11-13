ce8439e3402d2483a8c8ee546891de50f092724a
Imtiaz <imtiaz005123@gmail.com>
2020-11-13 11:06:22 +0600

    Add a class AttributeNames and add 2 final constant and create constructor in util\AttributeNames.java...

---
+++ web/src/main/java/com/imtiaz/util/AttributeNames.java
@@ -0,0 +1,13 @@
+package com.imtiaz.util;
+
+public final class AttributeNames {
+
+    // === Constants ===
+    public static final String MAIN_MESSAGE = "mainMessage";
+    public static final String RESULT_MESSAGE = "resultMessage";
+
+    // === Constructor ===
+    private AttributeNames(){
+
+    }
+}
