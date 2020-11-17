9570d3981040b6ccf509841dfc889b727e593bff
Imtiaz <imtiaz005123@gmail.com>
2020-11-17 11:21:54 +0600

    Add game-over.html then add 2 text, 1 result message, 1 play again link, 1 go to home link, 1 footer... RESTART and HOME link attributes add in the GameMappings.java class's constant part. Next part we work with the play again link..

--- web/src/main/java/com/imtiaz/util/GameMappings.java
+++ web/src/main/java/com/imtiaz/util/GameMappings.java
@@ -5,6 +5,8 @@ public final class GameMappings {
     // === Constants ===
     public static final String PLAY = "play";
     public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+    public static final String HOME = "/";
+    public static final String RESTART = "restart";
 
     // === Constructor ===
     private GameMappings(){
