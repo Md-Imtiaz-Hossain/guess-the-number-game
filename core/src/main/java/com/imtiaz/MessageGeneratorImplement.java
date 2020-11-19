fe6de1c0cf0028059165311f1cfd164edfab2135
Imtiaz <imtiaz005123@gmail.com>
2020-11-19 11:44:24 +0600

    Continue To compound messaging ,For final result messages- step1: create properties for all text in resultMessage methode. step2: translate to language. step3: insert into   getResultMessage methode in MessageGeneratorImplement class.

--- core/src/main/java/com/imtiaz/MessageGeneratorImplement.java
+++ core/src/main/java/com/imtiaz/MessageGeneratorImplement.java
@@ -13,6 +13,14 @@ public class MessageGeneratorImplement implements MessageGenerator {
 
     // === Constant ===
     private static final String MAIN_MESSAGE = "game.main.message";
+    private static final String WIN = "game.win";
+    private static final String LOSE = "game.lose";
+    private static final String INVALID_RANGE = "game.invalid.range";
+    private static final String FIRST_GUESS = "game.first.guess";
+    private static final String HIGHER = "game.higher";
+    private static final String LOWER = "game.lower";
+    private static final String REMAINING = "game.remaining";
+
 
 
     // === Fields ===
@@ -51,20 +59,20 @@ public class MessageGeneratorImplement implements MessageGenerator {
     public String getResultMessage() {
 
         if (game.isGameWon()){
-            return "You guess it! The number was " + game.getNumber();
+            return getMessage(WIN,  game.getNumber());
         }else if (game.isGameLost()){
-            return "You lost. The Number was " + game.getNumber();
+            return getMessage(LOSE, game.getNumber());
         }else if (!game.isValidNumberRange()){
-            return "Invalid Number Range";
+            return getMessage(INVALID_RANGE);
         }else if (game.getRemainingGuesses() == game.getGuessCount()){
-            return "What is your first guess ?";
+            return getMessage(FIRST_GUESS);
         }else{
-            String direction = "Lower";
+            String direction = getMessage(LOWER);
 
             if (game.getGuess() < game.getNumber()){
-                direction = "Higher";
+                direction = getMessage(HIGHER);
             }
-            return direction + "! You Have " + game.getRemainingGuesses() + " Guesses Left" ;
+            return getMessage(REMAINING,direction, game.getRemainingGuesses());
         }
     }
 
